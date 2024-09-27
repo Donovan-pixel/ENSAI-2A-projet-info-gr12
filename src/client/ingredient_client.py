a9af9c0a7ec319bf3e62a60aaecfe484807e35c9
LunaRiv <luna.riviere94@gmail.com>
2024-09-27 16:53:32 +0200

    IngredientClient à terminer

---
+++ src/client/ingredient_client.py
@@ -0,0 +1,29 @@
+import os
+import requests
+import dotenv
+from typing import List
+
+
+class IngredientClient:
+    """Fait un appel à l'endpoint Ingrédient"""
+
+    def __init__(self) -> None:
+        dotenv.load_dotenv()
+        self.__host = os.environ["WEBSERVICE_HOST"]
+
+    def get_ingredient(self) -> List[str]:
+        """
+        Retourne la liste des ingrédients
+        """
+
+        # Appel du Web service
+        req = requests.get(f"{self.__host}/list.php?i=list")
+
+        # Création d'une liste puis parcours du json pour ajouter tous les ingrédients à la liste
+        ingredients = []
+        if req.status_code == 200:
+            raw_types = req.json()["results"]
+            for t in raw_types:
+                ingredients.append(t["name"])
+
+        return sorted(ingredients)
