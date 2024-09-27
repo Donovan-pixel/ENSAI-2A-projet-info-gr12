309188b60cfb15b2255731386e9f4554f2ec3988
LunaRiv <luna.riviere94@gmail.com>
2024-09-27 14:25:33 +0200

    classe Utilisateur

---
+++ src/business_object/utilisateur.py
@@ -0,0 +1,26 @@
+class Utilisateur:
+    """
+    Classe représentant un Utilisateur
+
+    Attributs
+    ----------
+    idUtilisateur : int
+        identifiant
+    pseudo : str
+        pseudo de l'utilisateur
+    motDePasse : str
+        mot de passe de l'utilisateur
+    role : str
+        rôle de l'utilisateur
+    """
+
+    def __init__(self, idUtilisateur, pseudo, motDePasse, role):
+        """Constructeur"""
+        self.idUtilisateur = idUtilisateur
+        self.pseudo = pseudo
+        self.motDePasse = motDePasse
+        self.role = role
+
+    def __str__(self):
+        """Permet d'afficher les informations de l'utilisateur"""
+        return f"Utilisateur({self.pseudo}, {self.role})"
