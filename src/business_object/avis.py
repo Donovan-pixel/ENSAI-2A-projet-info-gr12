a7313b79081f93d2095669e0a6c76b74f4d482d4
LunaRiv <luna.riviere94@gmail.com>
2024-09-27 14:58:31 +0200

    classe Avis

---
+++ src/business_object/avis.py
@@ -0,0 +1,30 @@
+class Avis:
+    """
+    Classe représentant un avis
+
+    Attributs
+    ----------
+    idAvis : int
+        identifiant de l'avis
+    idUtilisateur : int
+        identifiant de l'utilisateur
+    idRecette : int
+        identifiant de la recette
+    note : float
+        note donnée par cet utilisateur pour la recette
+    commentaire : str
+        commentaire donnée par l'utilisateur pour la recette
+    """
+
+    def __init__(self, idAvis, idUtilisateur, idRecette, note, commentaire):
+        """Constructeur"""
+        self.idAvis = idAvis
+        self.idUtilisateur = idUtilisateur
+        self.idRecette = idRecette
+        self.note = note
+        self.commentaire = commentaire
+
+    def __str__(self):
+        """Permet d'afficher les informations d'un avis"""
+        return f"Avis donné par({self.idUtilisateur}, pour la recette {self.idRecette}"
+        f"avec la note {self.note} et le commentaire {self.commentaire})"
