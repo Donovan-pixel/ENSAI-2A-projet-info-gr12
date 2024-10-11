47797b2c52ecad8c0afae270db5974f5177d6efc
LunaRiv <luna.riviere94@gmail.com>
2024-10-11 16:11:13 +0200

    ajout classe ingredient_service

---
+++ src/service/ingredient_service.py
@@ -0,0 +1,34 @@
+from utils.log_decorator import log
+
+from business_object.ingredient import Ingredient
+from dao.ingredient_dao import IngredientDao
+
+
+class IngredientService:
+    """Classe contenant les méthodes de service des Ingrédients"""
+
+    @log
+    def ajouterNouvelIngredient(self, idIngredient, nom) -> Ingredient:
+        """Création d'un ingrédient à partir de ses attributs"""
+
+        nouvel_ingredient = Ingredient(
+            idIngredient=idIngredient,
+            nom=nom,
+        )
+
+        return nouvel_ingredient if IngredientDao().ajouterIngredient(nouvel_ingredient) else None
+
+    @log
+    def obtenirTousLesIngredients(self) -> list[Ingredient]:
+        """Lister tous les ingrédients"""
+        return IngredientDao().obtenirTousLesIngredients()
+
+    @log
+    def obtenirIngredientParId(self, idIngredient) -> Ingredient:
+        """Trouver un ingredient à partir de son id"""
+        return IngredientDao().obtenirIngredientParId(idIngredient)
+
+    @log
+    def supprimer(self, ingredient) -> bool:
+        """Supprimer un ingredient"""
+        return IngredientDao().supprimer(ingredient)
