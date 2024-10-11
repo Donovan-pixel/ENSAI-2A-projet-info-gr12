8aeebd90b6f1d7f429f4277a7394267eacc92818
LunaRiv <luna.riviere94@gmail.com>
2024-10-11 16:40:43 +0200

    correction classe utilisateur

--- src/business_object/utilisateur.py
+++ src/business_object/utilisateur.py
@@ -14,7 +14,9 @@ class Utilisateur:
         rôle de l'utilisateur
     """
 
-    def __init__(self, idUtilisateur, pseudo, motDePasse, role, recettes_favorites = [], ingredients_favoris = []):
+    def __init__(
+        self, idUtilisateur, pseudo, motDePasse, role, recettes_favorites=[], ingredients_favoris=[]
+    ):
         """Constructeur"""
         self.idUtilisateur = idUtilisateur
         self.pseudo = pseudo
