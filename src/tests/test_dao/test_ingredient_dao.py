f4b090ddc58b6bf8e0d548bb47d8b1f23df5bdf0
LunaRiv <luna.riviere94@gmail.com>
2024-10-22 12:39:40 +0200

    test ingredient dao

---
+++ src/tests/test_dao/test_ingredient_dao.py
@@ -0,0 +1,106 @@
+from unittest.mock import patch
+from unittest.mock import MagicMock
+
+from dao.ingredient_dao_dao import IngredientDao
+from business_object.ingredient import Ingredient
+
+
+@patch("dao.db_connection.DBConnection")
+def test_ajouterIngredient_succes(mock_db):
+
+    # GIVEN un ingrédient à ajouter et un base de données
+
+    mock_cursor = MagicMock()
+    mock_cursor.fetchone.return_value = [1]  # L'ingrédient est ajouté avec succès
+    mock_db().connection.__enter__().cursor.return_value = mock_cursor
+
+    ingredient = Ingredient(nom="Tomate")
+
+    # WHEN : on tente d'ajouter l'ingrédient
+
+    res = IngredientDao().ajouterIngredient(ingredient)
+
+    # THEN
+
+    assert res is True
+
+
+@patch("dao.db_connection.DBConnection")
+def test_ajouterIngredient_echec(mock_db):
+
+    # GIVEN
+
+    mock_cursor = MagicMock()
+    mock_cursor.fetchone.return_value = None  # L'ajout échoue
+    mock_db().connection.__enter__().cursor.return_value = mock_cursor
+
+    ingredient = Ingredient(nom="Tomate")
+
+    # WHEN
+
+    res = IngredientDao().ajouterIngredient(ingredient)
+
+    # THEN
+
+    assert res is False
+
+
+@patch("dao.db_connection.DBConnection")
+def test_obtenirTousLesIngredients_succes(mock_db):
+
+    # GIVEN
+
+    mock_cursor = MagicMock()
+    mock_cursor.fetchall.return_value = [
+        {"id_ingredient": 1, "nom": "Tomate"},
+        {"id_ingredient": 2, "nom": "Carotte"},
+    ]
+    mock_db().connection.__enter__().cursor.return_value = mock_cursor
+
+    # WHEN
+
+    res = IngredientDao().obternirTousLesIngredients()
+
+    # THEN
+
+    assert len(res) == 2
+    assert res[0].nom == "Tomate"
+    assert res[1].nom == "Carotte"
+
+
+@patch("dao.db_connection.DBConnection")
+def test_obtenirTousLesIngredients_echec(mock_db):
+
+    # GIVEN
+
+    mock_cursor = MagicMock()
+    mock_cursor.fetchall.return_value = []
+    mock_db().connection.__enter__().return_value = mock_db
+
+    # WHEN
+
+    res = IngredientDao().obternirTousLesIngredients()
+
+    # THEN
+
+    assert res == []
+
+
+@patch("dao.db_connection.DBConnection")
+def test_supprimerIngredient_succes(mock_db):
+
+    # GIVEN
+
+    mock_cursor = MagicMock()
+    mock_cursor.rowcount = 1  # Suppression réussie
+    mock_db().connection.__enter__().return_value = mock_cursor
+
+    ingredient = Ingredient(idIngredient=1, nom="Tomate")
+
+    # WHEN
+
+    res = IngredientDao().supprimerIngredient(ingredient)
+
+    # THEN
+
+    assert res is True
