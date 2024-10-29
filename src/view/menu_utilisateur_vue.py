877f6bdac6b02cbd8d97c022793426f759b3ab49
Melvin Bazeille <melvin.bazeille@eleve.ensai.fr>
2024-10-29 12:34:32 +0100

    Modif

--- src/view/menu_utilisateur_vue.py
+++ src/view/menu_utilisateur_vue.py
@@ -3,8 +3,6 @@ from InquirerPy import inquirer
 from view.vue_abstraite import VueAbstraite
 from view.session import Session
 
-from service.utilisateur_service import UtilisateurService
-from service.recette_service import RecetteService
 
 class MenuUtilisateurVue(VueAbstraite):
     """Vue du menu de l'utilisateur
@@ -47,24 +45,32 @@ class MenuUtilisateurVue(VueAbstraite):
             case "Se déconnecter":
                 Session().deconnexion()
                 from view.accueil.accueil_vue import AccueilVue
+
                 return AccueilVue()
 
             case "Voir mes recettes favorites":
                 from view.écrans.recettes_favorites_vue import RecettesFavoritesVue
+
                 return RecettesFavoritesVue()
 
             case "Afficher la liste des recettes":
                 from view.écrans.liste_des_recettes_vue import ListeDesRecettesVue
+
                 return ListeDesRecettesVue()
 
             case "Gérer mes ingrédients favoris/non désirés":
-                from view.écrans.ingredients_fav_non_desire_vue import IngredientsFavorisNonDesiresVue
+                from view.écrans.ingredients_fav_non_desire_vue import (
+                    IngredientsFavorisNonDesiresVue,
+                )
+
                 return IngredientsFavorisNonDesiresVue()
 
-            case "Obtenir des suggestions de recettes": 
+            case "Obtenir des suggestions de recettes":
                 from view.écrans.suggestion_vue import SuggestionVue
+
                 return SuggestionVue()
 
             case "Accéder à ma liste de courses":
                 from view.écrans.liste_de_courses_vue import ListeDeCoursesVue
+
                 return ListeDeCoursesVue()
