00549f8b633bf712e656897c03d02d6ecbce877c
Donovan-pixel <melvinbazeille@gmail.com>
2024-11-21 18:59:11 +0100

    Modif hash

--- src/utils/securite.py
+++ src/utils/securite.py
@@ -3,6 +3,8 @@ import hashlib
 
 def hash_password(password, sel=""):
     """Hachage du mot de passe"""
+    if salt is None:
+        salt = os.urandom(16).hex()
     password_bytes = password.encode("utf-8") + sel.encode("utf-8")
     hash_object = hashlib.sha256(password_bytes)
     return hash_object.hexdigest()
